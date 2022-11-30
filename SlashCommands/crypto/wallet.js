18e2f7d86acdf6371d70609391b586264b91edff
Grant Robinson <Chisdeal2014@hotmail.com>
2022-11-30 15:05:43 +0000

    Update wallet.js

--- SlashCommands/crypto/wallet.js
+++ SlashCommands/crypto/wallet.js
@@ -15,6 +15,8 @@ module.exports = {
         let znz = new RPCClient(client.config.walletnodes.znz.user, client.config.walletnodes.znz.pass, client.config.walletnodes.znz.ip, client.config.walletnodes.znz.port);
         let dogec = new RPCClient(client.config.walletnodes.dogec.user, client.config.walletnodes.dogec.pass, client.config.walletnodes.dogec.ip, client.config.walletnodes.dogec.port);
         let fls = new RPCClient(client.config.walletnodes.fls.user, client.config.walletnodes.fls.pass, client.config.walletnodes.fls.ip, client.config.walletnodes.fls.port);
+        let monk = new RPCClient(client.config.walletnodes.monk.user, client.config.walletnodes.monk.pass, client.config.walletnodes.monk.ip, client.config.walletnodes.monk.port);
+	    let owo = new RPCClient(client.config.walletnodes.owo.user, client.config.walletnodes.owo.pass, client.config.walletnodes.owo.ip, client.config.walletnodes.owo.port);
         const connection = client.sqlconn;
         
         connection.query(`SELECT * FROM profile_wallet WHERE userid='${interaction.user.id}'`, async function(err, results) {
@@ -38,6 +40,12 @@ module.exports = {
                 var walletflsaddress = await fls.call('getaddressesbyaccount', interaction.user.id);
                 var walletflsamount = await fls.call('getbalance', interaction.user.id);
                 var walletflscoldstakingamount = await fls.call('getcoldstakingbalance', interaction.user.id);
+                
+                var walletmonkaddress = await monk.call('getaddressesbyaccount', interaction.user.id);
+                var walletmonkamount = await monk.call('getbalance', interaction.user.id);
+                
+                var walletowoaddress = await owo.call('getaddressesbyaccount', interaction.user.id);
+                var walletowoamount = await owo.call('getbalance', interaction.user.id);
 
                 let embed = new MessageEmbed();
                 embed.setColor(0x9900FF)
@@ -46,7 +54,9 @@ module.exports = {
                     `PIVX WALLET:\nADDRESS: **${results[0].pivx}**\nBALANCE: **${walletpivxamount}**\nCOLDSTAKING BALANCE: **${walletpivxcoldstakingamount}**\n\n` +
                     `ZENZO WALLET:\nADDRESS: **${results[0].znz}**\nBALANCE: **${walletznzamount}**\nCOLDSTAKING BALANCE: **${walletznzcoldstakingamount}**\n\n` +
                     `DOGECASH WALLET:\nADDRESS: **${results[0].dogec}**\nBALANCE: **${walletdogecamount}**\nCOLDSTAKING BALANCE: **${walletdogeccoldstakingamount}**\n\n` +
-                    `FLITS WALLET:\nADDRESS: **${results[0].fls}**\nBALANCE: **${walletflsamount}**\nCOLDSTAKING BALANCE: **${walletflscoldstakingamount}**\n\n`
+                    `FLITS WALLET:\nADDRESS: **${results[0].fls}**\nBALANCE: **${walletflsamount}**\nCOLDSTAKING BALANCE: **${walletflscoldstakingamount}**\n\n` +
+                    `MONK WALLET:\nADDRESS: **${results[0].monk}**\nBALANCE: **${walletmonkamount}**\n\n` +
+                    `OWO WALLET:\nADDRESS: **${results[0].owo}**\nBALANCE: **${walletowoamount}**\n\n`
                 )
                 embed.setFooter("Requested by " + interaction.user.username, client.user.avatarURL)
                 embed.setTimestamp()
