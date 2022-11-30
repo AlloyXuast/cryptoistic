9c25dedc47a22521e44d2f34eead2829e0fbab52
Grant Robinson <Chisdeal2014@hotmail.com>
2022-11-30 14:51:57 +0000

    Update node-api.js

--- events/node-api.js
+++ events/node-api.js
@@ -44,6 +44,24 @@ client.on("ready", async () => {
             blocks: 0,
             peers: 0,
             lastuptime: 0
+        },
+        "monk": { 
+            status: "offline",
+            synced: false,
+            behind: 0,
+            progress: "0%",
+            blocks: 0,
+            peers: 0,
+            lastuptime: 0
+        },
+        "owo": { 
+            status: "offline",
+            synced: false,
+            behind: 0,
+            progress: "0%",
+            blocks: 0,
+            peers: 0,
+            lastuptime: 0
         }
     }
 
@@ -51,7 +69,9 @@ client.on("ready", async () => {
     const RPCPIVX = new RPCClient(client.config.walletnodes.pivx.user, client.config.walletnodes.pivx.pass, client.config.walletnodes.pivx.ip, client.config.walletnodes.pivx.port)
     const RPCFLS = new RPCClient(client.config.walletnodes.fls.user, client.config.walletnodes.fls.pass, client.config.walletnodes.fls.ip, client.config.walletnodes.fls.port)
     const RPCDOGEC = new RPCClient(client.config.walletnodes.dogec.user, client.config.walletnodes.dogec.pass, client.config.walletnodes.dogec.ip, client.config.walletnodes.dogec.port)
-
+    const RPCMONK = new RPCClient(client.config.walletnodes.monk.user, client.config.walletnodes.monk.pass, client.config.walletnodes.monk.ip, client.config.walletnodes.monk.port)
+    const RPCOWO = new RPCClient(client.config.walletnodes.owo.user, client.config.walletnodes.owo.pass, client.config.walletnodes.owo.ip, client.config.walletnodes.owo.port)
+    
     const globalresznz = await fetch('https://chainz.cryptoid.info/znz/api.dws?q=getblockcount');
     const globaldataznz = await globalresznz.text();
     
@@ -63,6 +83,12 @@ client.on("ready", async () => {
     
     const globalrespivx = await fetch('https://pivx.flitswallet.app/api/v1/');
     const globaldatapivx = await globalrespivx.json();
+    
+    const globalresmonk = await fetch('https://explorer.decenomy.net/coreapi/v1/coins/MONK/blocks?perPage=1&page=1');
+    const globaldatamonk = await globalresmonk.json();
+    
+    const globalresowo = await fetch('https://explorer.decenomy.net/coreapi/v1/coins/OWO/blocks?perPage=1&page=1');
+    const globaldataowo = await globalresowo.json();
     
     app.get('/', (req, res) => {
         res.header("Access-Control-Allow-Origin", "*");
@@ -132,6 +158,22 @@ client.on("ready", async () => {
 
     app.get('/dogec/cstx', function (req, res) {
         RPCDOGEC.call('listcoldutxos').then(otx => {
+            res.json(otx)
+        }).catch(err => {
+            res.json(node.dogec)
+        })
+    })
+    
+    app.get('/monk/tx', function (req, res) {
+        RPCMONK.call('listtransactions').then(otx => {
+            res.json(otx)
+        }).catch(err => {
+            res.json(node.dogec)
+        })
+    })
+    
+    app.get('/owo/tx', function (req, res) {
+        RPCOWO.call('listtransactions').then(otx => {
             res.json(otx)
         }).catch(err => {
             res.json(node.dogec)
@@ -328,6 +370,107 @@ client.on("ready", async () => {
         console.error("[ZNZ] RPC Error: Node status changed to " + node.znz.status)
         setTimeout(checkRPCZNZ, sec(60))
       }
+    
+    
+    
+    
+    
+     
+     function checkRPCMONK () {
+        RPCMONK.call('getblockcount').then(blocks => {
+          node.monk.status = "online"
+          console.log("[MONK] RPC Response: " + blocks + ", Node status changed to " + node.monk.status)
+          updateRPCMONK()
+        }).catch(rpcErrorMONK)
+      }
+      
+      checkRPCMONK()
+      
+      /* Fired repeatedly, updates node information periodically, if online */
+      function updateRPCMONK() {
+        if (node.monk.status === "online") {
+          RPCMONK.call('getblockcount').then(blocks => {
+            if (blocks == globaldatamonk.response[0].height) {
+                node.monk.synced = true
+                node.monk.behind = 0
+                node.monk.progress = "100%"
+            } else if (globaldatamonk.response[0].height - blocks < -1) {
+                node.monk.synced = true
+                node.monk.behind = 0
+                node.monk.progress = "100%"
+            } else {
+                node.monk.synced = false
+                node.monk.behind = globaldatamonk.response[0].height - blocks
+                node.monk.progress = (100.0 * blocks / globaldatamonk.response[0].height).toFixed(2)
+            }
+            node.monk.blocks = blocks
+            RPCMONK.call('getconnectioncount').then(conns => {
+              node.monk.lastuptime = Math.floor(Date.now() / 1000);
+              node.monk.peers = conns
+              console.log("[MONK] RPC Updated: " + blocks + " blocks and " + conns + " peers")
+            }).catch(rpcErrorMONK)
+          }).catch(rpcErrorMONK)
+        }
+      }
+      
+      setInterval(updateRPCMONK, sec(10))
+      
+      /* Fired when the Monitor catches an RPC error */
+      function rpcErrorMONK () {
+        node.monk.status = "offline"
+        console.error("[MONK] RPC Error: Node status changed to " + node.monk.status)
+        setTimeout(checkRPCMONK, sec(60))
+      }
+    
+    
+    
+    
+      function checkRPCOWO () {
+        RPCOWO.call('getblockcount').then(blocks => {
+          node.owo.status = "online"
+          console.log("[OWO] RPC Response: " + blocks + ", Node status changed to " + node.owo.status)
+          updateRPCOWO()
+        }).catch(rpcErrorOWO)
+      }
+      
+      checkRPCOWO()
+      
+      /* Fired repeatedly, updates node information periodically, if online */
+      function updateRPCOWO() {
+        if (node.owo.status === "online") {
+          RPCOWO.call('getblockcount').then(blocks => {
+            if (blocks == globaldataowo.response[0].height) {
+                node.owo.synced = true
+                node.owo.behind = 0
+                node.owo.progress = "100%"
+            } else if (globaldataowo.response[0].height - blocks < -1) {
+                node.owo.synced = true
+                node.owo.behind = 0
+                node.owo.progress = "100%"
+            } else {
+                node.owo.synced = false
+                node.owo.behind = globaldataowo.response[0].height - blocks
+                node.owo.progress = (100.0 * blocks / globaldataowo.response[0].height).toFixed(2)
+            }
+            node.owo.blocks = blocks
+            RPCOWO.call('getconnectioncount').then(conns => {
+              node.owo.lastuptime = Math.floor(Date.now() / 1000);
+              node.owo.peers = conns
+              console.log("[OWO] RPC Updated: " + blocks + " blocks and " + conns + " peers")
+            }).catch(rpcErrorOWO)
+          }).catch(rpcErrorOWO)
+        }
+      }
+      
+      setInterval(updateRPCOWO, sec(10))
+      
+      /* Fired when the Monitor catches an RPC error */
+      function rpcErrorOWO () {
+        node.owo.status = "offline"
+        console.error("[OWO] RPC Error: Node status changed to " + node.owo.status)
+        setTimeout(checkRPCOWO, sec(60))
+      }
+     
            
       /* Converts seconds into miliseconds */
       function sec (s) {
